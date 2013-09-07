0f1c068c42a1694798e6336f3b5309f1ae23987c
Linus Torvalds <torvalds@linux-foundation.org>
2013-09-07 09:48:41 -0700

    Revert "Input: introduce BTN/ABS bits for drums and guitars"
    This reverts commits 61e00655e9cb, 73f8645db191 and 8e22ecb603c8:
      "Input: introduce BTN/ABS bits for drums and guitars"
      "HID: wiimote: add support for Guitar-Hero drums"
      "HID: wiimote: add support for Guitar-Hero guitars"
    
    The extra new ABS_xx values resulted in ABS_MAX no longer being a
    power-of-two, which broke the comparison logic.  It also caused the
    ioctl numbers to overflow into the next byte, causing problems for that.
    
    We'll try again for 3.13.
    
    Reported-by: Markus Trippelsdorf <markus@trippelsdorf.de>
    Reported-by: Linus Torvalds <torvalds@linux-foundation.org>
    Acked-by: David Herrmann <dh.herrmann@gmail.com>
    Acked-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Cc: Benjamin Tissoires <benjamin.tissoires@gmail.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/hid/hid-wiimote.h
+++ drivers/hid/hid-wiimote.h
@@ -88,8 +88,6 @@ enum wiimote_exttype {
 	WIIMOTE_EXT_CLASSIC_CONTROLLER,
 	WIIMOTE_EXT_BALANCE_BOARD,
 	WIIMOTE_EXT_PRO_CONTROLLER,
-	WIIMOTE_EXT_GUITAR_HERO_DRUMS,
-	WIIMOTE_EXT_GUITAR_HERO_GUITAR,
 	WIIMOTE_EXT_NUM,
 };
 
@@ -129,7 +127,6 @@ struct wiimote_state {
 
 	/* calibration data */
 	__u16 calib_bboard[4][3];
-	__u8 pressure_drums[7];
 };
 
 struct wiimote_data {
