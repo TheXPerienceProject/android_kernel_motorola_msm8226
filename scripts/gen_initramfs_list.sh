e659dbceaa0318c76b75b1d82beff8a586e429c9
Daniel M. Weeks <dan@danweeks.net>
2014-03-03 15:38:28 -0800

    scripts/gen_initramfs_list.sh: fix flags for initramfs LZ4 compression
    LZ4 as implemented in the kernel differs from the default method now
    used by the reference implementation of LZ4.  Until the in-kernel method
    is updated to support the new default, passing the legacy flag (-l) to
    the compressor is necessary.  Without this flag the kernel-generated,
    LZ4-compressed initramfs is junk.
    
    Kyungsik said:
    
    : It seems that lz4 supports legacy format with the same option as lz4c
    : does.  Just looking at the first few bytes of lz4 compressed image, we can
    : see whether it is new format or not.
    :
    : It shows new format magic number without this patch.  New format magic
    : number is 0x184d2204.
    :
    : $ hexdump -C ./initramfs_data.cpio.lz4 |more
    : 00000000  04 22 4d 18 64 70 b9 69 (Little Endian)
    : ...
    :
    : Currently kernel supports legacy format only.
    
    Change-Id: I23674b891994c8938c6a372a392649b1d36c1fc9
    Signed-off-by: Daniel M. Weeks <dan@danweeks.net>
    Cc: Michal Marek <mmarek@suse.cz>
    Acked-by: Kyungsik Lee <kyungsik.lee@lge.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- scripts/gen_initramfs_list.sh
+++ scripts/gen_initramfs_list.sh
@@ -258,7 +258,7 @@ case "$arg" in
                 && compr="lzop -9 -f"
 		echo "$output_file" | grep -q "\.lz4$" \
                 && [ -x "`which lz4 2> /dev/null`" ] \
-                && compr="lz4 -9 -f"
+                && compr="lz4 -l -9 -f"
 
 		echo "$output_file" | grep -q "\.cpio$" && compr="cat"
 		shift
