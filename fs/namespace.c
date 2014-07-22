92533e5a956f8046f552abfd26669f3002b928a2
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-07-22 04:57:05 -0500

    Reduce Disk Writes With noatime, nodiratime noatime, nodiratime are disk mount options to disable access time information updates onto every file or directory the system access. By using these options, disk writing activity can be reduce significantly.
        noatime - Disable access time update of files in HDD.
        nodiratime - Disable access time update of directories in HDD
    ****************************************************************************************
    
    [Explanation]
    Turns out our save files, for each of our files, a parameter called "atime" or "access time". Total, that each time you access (read) a file, the value "atime" is updated. So we know which was the last time we agreed / we read a particular file.
    
    Of course, this is an unwanted behavior: It turns out that we do read each disk, we need to write something to disk! (Update the value "access time"). This seems absurd! To read we have to write.
    
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -2221,10 +2221,6 @@ long do_mount(const char *dev_name, const char *dir_name,
 	if (retval)
 		goto dput_out;
 
-	/* Default to relatime unless overriden */
-	if (!(flags & MS_NOATIME))
-		mnt_flags |= MNT_RELATIME;
-
 	/* Separate the per-mountpoint flags */
 	if (flags & MS_NOSUID)
 		mnt_flags |= MNT_NOSUID;
@@ -2232,9 +2228,7 @@ long do_mount(const char *dev_name, const char *dir_name,
 		mnt_flags |= MNT_NODEV;
 	if (flags & MS_NOEXEC)
 		mnt_flags |= MNT_NOEXEC;
-	if (flags & MS_NOATIME)
 		mnt_flags |= MNT_NOATIME;
-	if (flags & MS_NODIRATIME)
 		mnt_flags |= MNT_NODIRATIME;
 	if (flags & MS_STRICTATIME)
 		mnt_flags &= ~(MNT_RELATIME | MNT_NOATIME);
