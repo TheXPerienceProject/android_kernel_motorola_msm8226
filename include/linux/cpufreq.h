00c32c2bc5bcee65421ef0c5e6d5f9bbc1b06d5b
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-12-22 22:17:41 -0600

    Initial porting of Intellimm from nexus devices Autor : Faux123
    Change-Id: If0a9e0c4e96ec965727f7dcea7bc23967c87a316
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -381,6 +381,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_XPERIENCE)
 extern struct cpufreq_governor cpufreq_gov_xperience;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_xperience)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
+extern struct cpufreq_governor cpufreq_gov_intellimm;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_intellimm)
 #endif
 
 
