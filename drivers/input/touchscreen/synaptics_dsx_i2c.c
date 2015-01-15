d4c041fbf3d762d757a7019f7a2b1c9c6de1296a
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2015-01-15 17:37:11 -0600

    s2w/dt2w: separate PocketMode and more fixes
    Change-Id: I42531002b8e276a62f650b20f5279d36261148ee
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- drivers/input/touchscreen/synaptics_dsx_i2c.c
+++ drivers/input/touchscreen/synaptics_dsx_i2c.c
@@ -2154,7 +2154,9 @@ static int synaptics_rmi4_irq_enable(struct synaptics_rmi4_data *rmi4_data,
 			return retval;
 		}
 
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
 		irq_set_irq_wake(rmi4_data->irq, 1);
+#endif
 
 		dev_dbg(&rmi4_data->i2c_client->dev,
 				"%s: Started irq thread\n", __func__);
@@ -2166,7 +2168,9 @@ static int synaptics_rmi4_irq_enable(struct synaptics_rmi4_data *rmi4_data,
 			free_irq(rmi4_data->irq, rmi4_data);
 			rmi4_data->irq_enabled = false;
 
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP 
 		irq_set_irq_wake(rmi4_data->irq, 0);
+#endif
 
 		dev_dbg(&rmi4_data->i2c_client->dev,
 				"%s: Stopped irq thread\n", __func__);
@@ -3729,10 +3733,12 @@ static int synaptics_rmi4_suspend(struct device *dev)
 	return 0;
 }
 
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
 void touch_suspend(void)
 {
 	synaptics_rmi4_suspend(&(exp_fn_ctrl.rmi4_data_ptr->input_dev->dev));
 }
+#endif
 
  /**
  * synaptics_rmi4_resume()
@@ -3802,10 +3808,12 @@ static int synaptics_rmi4_resume(struct device *dev)
 	return 0;
 }
 
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
 void touch_resume(void)
 {
 	 synaptics_rmi4_resume(&(exp_fn_ctrl.rmi4_data_ptr->input_dev->dev));
 }
+#endif
 
 static const struct dev_pm_ops synaptics_rmi4_dev_pm_ops = {
 	.suspend = synaptics_rmi4_suspend,
