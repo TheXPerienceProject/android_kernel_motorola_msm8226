b46c54f69aa1aab58ae330a661dcb67bbbe66db0
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2015-03-19 20:20:31 -0600

    board-8226: Initialize intelli_thermal engine
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- arch/arm/mach-msm/board-8226.c
+++ arch/arm/mach-msm/board-8226.c
@@ -178,8 +178,8 @@ void __init msm8226_add_drivers(void)
 	else
 		msm_clock_init(&msm8226_clock_init_data);
 	tsens_tm_init_driver();
-#ifndef CONFIG_INTELLI_THERMAL
-	msm_thermal_device_init();
+#ifdef CONFIG_INTELLI_THERMAL
+	msm_thermal_device_init(NULL);
 #endif
 #ifdef CONFIG_LCD_KCAL
 	add_lcd_kcal_devices();
