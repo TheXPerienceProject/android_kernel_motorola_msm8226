afe9734c5b0c5a19ffa5d8d5ded9eabf89651202
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2015-03-19 21:18:45 -0600

    pdated intelli thermal to use on MSM8226 board
    Change-Id: Iac00ab14df40a02fe300234704f9efe49430c8b1
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- arch/arm/mach-msm/board-8226.c
+++ arch/arm/mach-msm/board-8226.c
@@ -126,7 +126,10 @@ void __init msm8226_add_drivers(void)
 	else
 		msm_clock_init(&msm8226_clock_init_data);
 	tsens_tm_init_driver();
-#ifndef CONFIG_INTELLI_THERMAL
+
+#ifdef CONFIG_INTELLI_THERMAL
+	msm_thermal_init(NULL);
+#else
 	msm_thermal_device_init();
 #endif
 }
