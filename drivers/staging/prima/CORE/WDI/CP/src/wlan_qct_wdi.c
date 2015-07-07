15c748b4cb168c293e143d2d373eb39a878da441
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2015-07-06 20:18:47 -0500

    PRIMA_WLAN: WDI: fix ERROR: "wcnss_device_is_shutdown" [drivers/staging/prima/wlan.ko] undefined! and other issues Fix issue with build
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- drivers/staging/prima/CORE/WDI/CP/src/wlan_qct_wdi.c
+++ drivers/staging/prima/CORE/WDI/CP/src/wlan_qct_wdi.c
@@ -1005,7 +1005,7 @@ static char *WDI_getReqMsgString(wpt_uint16 wdiReqMsgId)
  @see
  @return Result of the function call
 */
-static char *WDI_getRespMsgString(wpt_uint16 wdiRespMsgId)
+inline char *WDI_getRespMsgString(wpt_uint16 wdiRespMsgId)
 {
   switch (wdiRespMsgId)
   {
